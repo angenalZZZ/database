005f36fc3acb453e3c61d768bd788ca6af8b4198
Angenal <angenal@hotmail.com>
2020-11-09 15:27:43 +0800

    Update command-line-json.sql

--- Database_Files/SQLite/command-line-json.sql
+++ Database_Files/SQLite/command-line-json.sql
@@ -10,3 +10,13 @@
 -- > .quit
 
 select * from logs limit 10 offset 0;
+
+CREATE TABLE [logtest] (
+	[Id] integer NOT NULL PRIMARY KEY AUTOINCREMENT, 
+	[Code] varchar(50) NOT NULL, 
+	[Type] int NOT NULL, 
+	[Message] nvarchar(2000) NOT NULL, 
+	[Account] varchar(36), 
+	[CreateTime] datetime NOT NULL, 
+	[CreateUser] varchar(36)
+);
